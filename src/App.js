bcfda4d1f42cc43836e5b19841b3665d43abd7b1
Ashwin <8831182+ashwinr63@users.noreply.github.com>
2022-06-08 17:28:02 -0400

    Adding CSS and adding navigation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route} from 'react-router-dom'
+
+import Navigation from  './routes/navigation/navigation.component'
 import Home from './routes/home/home.component'
 
-
 const Shop = () => {
-  return <h1> I'm Shop Component</h1> 
+  return <h1> I'm Shop Component</h1>
 }
 
 
 const App = () => {
-  
+
   return (
 
     <Routes>
-      <Route path="/" element={<Home />} >
+      <Route path="/" element={<Navigation />} >
+        <Route index element={<Home />} />
         <Route path="Shop" element={<Shop />} />
       </Route>
     </Routes>
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,31 @@
+import { Fragment } from "react";
+
+import { Outlet, Link } from "react-router-dom"
+
+import { ReactComponent as ShoppingLogo } from '../../assests/crown.svg'
+
+import './navigation.styles.scss'
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className="navigation">
+                <Link className="logo-container" to='/'>
+                    <ShoppingLogo className="logo" />
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to='/shop'>
+                        Shop
+                    </Link>
+                </div>
+                <h1></h1>
+            </div>
+            <Outlet />
+        </Fragment>
+    )
+
+}
+
+
+
+export default Navigation; 
