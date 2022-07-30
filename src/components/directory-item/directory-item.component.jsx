5b07e363989133b791738eecbe35054bd6e4fe22
Ashwin <8831182+ashwinr63@users.noreply.github.com>
2022-07-30 01:29:13 -0400

    Last touches on working structures

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,20 +1,23 @@
-import {BackgroundImage, DirectoryBody, DirectoryItemContainer} from './directory-item.styles';
 
+import { useNavigate } from 'react-router-dom'
+import { BackgroundImage, DirectoryBody, DirectoryItemContainer } from './directory-item.styles';
 const DirectoryItem = ({ category }) => {
 
-    const {imageUrl, title} = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate()
+  const onNavigateHandler = () => navigate(route);
 
-    return (
-        <DirectoryItemContainer>
-        <BackgroundImage style={{
-          backgroundImage: `url(${imageUrl})`
-        }}/>
-        <DirectoryBody>
-          <h2> {title} </h2>
-          <p> Shop Now</p>
-        </DirectoryBody>
-      </DirectoryItemContainer>
-    )
+  return (
+    <DirectoryItemContainer onClick={onNavigateHandler}>
+      <BackgroundImage style={{
+        backgroundImage: `url(${imageUrl})`
+      }} />
+      <DirectoryBody>
+        <h2> {title} </h2>
+        <p> Shop Now</p>
+      </DirectoryBody>
+    </DirectoryItemContainer>
+  )
 
 }
 
