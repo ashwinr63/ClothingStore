d02d628339e46fabdf84713c2675ba3f8c06621c
Ashwin <8831182+ashwinr63@users.noreply.github.com>
2023-05-09 21:58:41 -0400

    Additional Packages installed while removing NPM for YARN

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
-
+import { Provider } from 'react-redux';
 import App from './App';
-import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
-import { CartProvider } from './contexts/cart.context'
+import { CartProvider } from './contexts/cart.context';
+import { store } from './store/store';
 import './index.scss';
 
+const rootElement = document.getElementById('root');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+render(
   <React.StrictMode>
+    <Provider store ={store}>
     <BrowserRouter>
-    <UserProvider>
-      <CategoriesProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </CategoriesProvider>
-      </UserProvider>
+      {/* <UserProvider> */}
+        <CategoriesProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </CategoriesProvider>
+      {/* </UserProvider> */}
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,5 +1,9 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
 
-export const rootReducer = combineReducers({
-    
-})
+import { userReducer } from './user/user.reducer';
+import { categoriesReducer } from './categories/category.reducer';
+
+export const rootReducer = combineReducers({ 
+  user: userReducer,
+  categories: categoriesReducer
+});
