188bbde4f66aff7166471ee9e721b9ed8f8734ba
Ashwin <8831182+ashwinr63@users.noreply.github.com>
2023-12-03 02:53:11 -0500

    Update context and store

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -8,9 +8,11 @@ const Category = () => {
 
     const { category } = useParams();
     const categoriesMap = useSelector(selectCategoriesMap)
+    console.log('render/re-rendering category')
     const [products, setProducts] = useState(categoriesMap[category])
-
+   
     useEffect(() => {
+        console.log('effect fired calling setProducts')
         setProducts(categoriesMap[category])
     }, [category, categoriesMap])
 
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,15 +3,15 @@ import { useDispatch } from 'react-redux'
 import CategoriesPreview from '../categories-preview/categories-preview.component.jsx'
 import Category from '../category/category.component.jsx'
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils.js'
-import { setCategoriesMap } from '../../store/categories/category.action.js'
+import { setCategories } from '../../store/categories/category.action.js'
 import { Routes, Route } from 'react-router-dom'
 
 const Shop = () => {
     const dispatch = useDispatch();
     useEffect(() => {
 		const getCategoriesMap = async () => {
-			const categoryMap = await getCategoriesAndDocuments('categories');
-			dispatch(setCategoriesMap(categoryMap));
+			const categoriesArray = await getCategoriesAndDocuments('categories');
+			dispatch(setCategories(categoriesArray));
 		};
 
 		getCategoriesMap();
